135bd5a34b489d77f28b7e0a48ad329ce751d003
brnoschsaloli <brenosso@al.insper.edu.br>
2023-09-01 15:26:00 -0300

    darkmode

--- templates/getit.js
+++ templates/getit.js
@@ -4,6 +4,44 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+let darkbutton = document.getElementById("dark-button")
+let body = document.body
+let titulo = document.getElementById("titulo");
+let descricao = document.getElementById("descricao")
+let formbutton = document.getElementById("form-button")
+let appbar = document.getElementById("appbar")
+
+darkbutton.addEventListener( 'change', function() {
+  if(this.checked) {
+      localStorage.setItem('dark',this.checked);
+
+      body.classList.add('dark-mode')
+      titulo.classList.add('dark-mode-form')
+      descricao.classList.add('dark-mode-form')
+      formbutton.classList.add('btn-dark')
+      appbar.classList.add('appbar-dark')
+      darkbutton.classList.add('btn-dark')
+  } else{
+      localStorage.removeItem('dark');
+      body.classList.remove('dark-mode')
+      titulo.classList.remove('dark-mode-form')
+      descricao.classList.remove('dark-mode-form')
+      formbutton.classList.remove('btn-dark')
+      appbar.classList.remove('appbar-dark')
+      darkbutton.classList.remove('btn-dark')  
+  }
+  });
+
+if(localStorage.getItem('dark')) {
+  darkbutton.checked = "true"
+  body.classList.add('dark-mode')
+  titulo.classList.add('dark-mode-form')
+  descricao.classList.add('dark-mode-form')
+  formbutton.classList.add('btn-dark')
+  appbar.classList.add('appbar-dark')
+  darkbutton.classList.add('btn-dark')
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   // Faz textarea aumentar a altura automaticamente
   // Fonte: https://www.geeksforgeeks.org/how-to-create-auto-resize-textarea-using-javascript-jquery/#:~:text=It%20can%20be%20achieved%20by,height%20of%20an%20element%20automatically.
